f0e59d144bab17b9eacd61c151e816ff9be56c34
Juan <judasane@gmail.com>
2018-01-07 13:31:53 -0500

    vacío

--- assets/js/init.js
+++
@@ -1,7 +0,0 @@
-
-/* var sidel = document.querySelector('.sidenav');
-var instance = new M.Sidenav(sidel); */
-
-
-/* var tabel = document.querySelector('.tabs');
-var instance = new M.Tabs(tabel); */
